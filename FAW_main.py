02197d75be07ebdf06d31e5b41405ecbe1658754
TzashR <tzachirotbain@gmail.com>
2020-12-23 09:51:24 +0200

    deleted unecessary function

--- FAW_main.py
+++ FAW_main.py
@@ -87,11 +87,3 @@ def main():
 if __name__ == '__main__':
     main()
 
-def get_n_params(model):
-    pp=0
-    for p in list(model.parameters()):
-        nn=1
-        for s in list(p.size()):
-            nn = nn*s
-        pp += nn
-    return pp
