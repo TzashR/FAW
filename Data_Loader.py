58bf733819c41cd9bfb1abd4dc8b57e82e1e3c0a
TzashR <tzachirotbain@gmail.com>
2021-03-22 18:02:18 +0200

    refactoring code

--- Data_Loader.py
+++ Data_Loader.py
@@ -1,106 +1,7 @@
-import os
-import random
 import cv2
 import numpy as np
-import pandas as pd
 import torch
 from torch.utils.data import DataLoader
-
-def fix_id(id, i=-1):
-    # check_id(i, id)
-    return (id + 50) // 100 * 100
-
-
-def check_id(i, id):
-    # print(i, id)
-    id1 = (id // 100) * 100
-    # print(id1)
-    assert id == id1 or (id - 4) == id1 or (id - 8) == id1 or (id - 96) == id1 or (id - 92) == id1
-
-
-def make_actual_file_path(measurement_path, root_folder):
-    '''
-    @param measurement_path: A file path from photo conversion excel
-    @param root_folder: Folder containing images
-    @return: path to actual file in the folder
-    '''
-
-    splitted_measurement_path = measurement_path.split("/")
-    actual_path = os.path.join(root_folder, splitted_measurement_path[2], splitted_measurement_path[3])
-
-    return actual_path
-
-
-# TODO I need to get the index for each image
-def process_data(reports_df, images_df, images_dir, bad_shaped_images = None):
-    '''
-
-    @param reports_table:
-    @param images_table:
-    @param images_dir:
-    @bad_shaped_images: a set of file paths to images with bad shape. They will be ignored
-    @return: -
-        usable : { report_id: (infest_level, [image_file_path])}
-        seedling_reports : {report id: [image_file_path}
-        number of missing images (in reports but not in image dir)
-        number of reportless images (in dir but not in reoprts)
-        number of total images in dir
-
-
-    '''
-    reports_dic = {}
-    seedlings_dic = {}
-
-    # creates dictionary for reports (one for usable  and on for seedlings)
-    for i in range(1, len(reports_df)):
-        report_id = fix_id(fix_id(reports_df[0][i]))
-        if reports_df[9][i] == 'Seedling':
-            seedlings_dic[report_id] = []
-        else:
-            reports_dic[report_id] = {'infest_level': reports_df[7][i], 'images': []}
-
-    num_images_not_in_reports = 0
-    num_missing_image_files = 0
-    total_images = 0
-    usable_index = 0
-    usable_reports_lst = []
-    index_to_label = {}
-
-    # assigns image files to their respective report in the dictionaries
-    for i in range(1, len(images_df)):
-        measurement_id = fix_id(images_df[0][i])
-        image_file_path = make_actual_file_path(images_df[1][i], images_dir)
-
-        if not os.path.isfile(image_file_path):
-            num_missing_image_files += 1
-            continue
-        elif bad_shaped_images is not None and image_file_path in bad_shaped_images:
-            continue
-        if measurement_id in reports_dic:
-            usable_reports_lst.append(image_file_path)
-            reports_dic[measurement_id]['images'].append((image_file_path, usable_index))
-            index_to_label[usable_index] = reports_dic[measurement_id]['infest_level']
-            usable_index += 1
-        elif measurement_id in seedlings_dic:
-            seedlings_dic[measurement_id].append(image_file_path)
-        else:
-            num_images_not_in_reports += 1
-        total_images += 1
-    return (reports_dic, usable_reports_lst, index_to_label, seedlings_dic,
-            num_missing_image_files, num_images_not_in_reports, total_images)
-
-
-def faw_transform(img):
-    new_img = np.zeros((img.shape[0], img.shape[1], 4))
-    new_img[:, :, :3] = img / 255.0
-    # adds GRVI channel. (r+g)/(r-g)
-    new_img[:, :, 3] = (new_img[:, :, 1] - new_img[:, :, 0]) / (new_img[:, :, 0] + new_img[:, :, 1] + .00001)
-    if new_img.shape[0] < new_img.shape[1]:  # height first
-        new_img = np.transpose(new_img, (1, 0, 2))
-    new_img = np.transpose(new_img, (2, 0, 1))
-    assert (new_img.shape == (4,1600,960)), f'new_img shape {new_img.shape}, img shape {img.shape} img = {img}'
-    return new_img
-
 
 class FawDataset(torch.utils.data.Dataset):
     def __init__(self, images, labels, transform=(lambda img: img)):
@@ -124,31 +25,16 @@ class FawDataset(torch.utils.data.Dataset):
     def __len__(self):
         len(self.images.items())
 
-def make_batches(batch_size, reports_dic, min_images=5, seed=0):
-    # list of all reports with at least min_images images
-    usable_reports = list(filter(lambda report: len(report[1]['images']) >= min_images, reports_dic.items()))
-    random.shuffle(usable_reports)
-    n = len(usable_reports)
-    num_of_batches,remainder = n//batch_size, n%batch_size
-
-    #batches at this point are reports, we need to convert them to image indices
-    batch_reports = [usable_reports[i:i+batch_size] for i in range(num_of_batches)]
-    if remainder>0: batch_reports.append(usable_reports[-remainder:])
-
-    batches = []
-#converts all reports in batches to a list of image indices
-    for rep_batch in batch_reports:
-        batch_indices = []
-        for report in rep_batch:
-            for image_tuple in report[1]['images']: #image tuple contains path, index
-                batch_indices.append(image_tuple[1])
-        batches.append(batch_indices)
-    return batches
-
-
-def faw_batch_sampler(batches):
-    for i in range(len(batches)):
-        yield batches[i]
+def faw_transform(img):
+    new_img = np.zeros((img.shape[0], img.shape[1], 4))
+    new_img[:, :, :3] = img / 255.0
+    # adds GRVI channel. (r+g)/(r-g)
+    new_img[:, :, 3] = (new_img[:, :, 1] - new_img[:, :, 0]) / (new_img[:, :, 0] + new_img[:, :, 1] + .00001)
+    if new_img.shape[0] < new_img.shape[1]:  # height first
+        new_img = np.transpose(new_img, (1, 0, 2))
+    new_img = np.transpose(new_img, (2, 0, 1))
+    assert (new_img.shape == (4,1600,960)), f'new_img shape {new_img.shape}, img shape {img.shape} img = {img}'
+    return new_img
 
 # %%
 ##for testing
--- FAW_train.py
+++ FAW_train.py
@@ -2,26 +2,14 @@ import argparse
 import pandas as pd
 import torch.optim
 from torch.utils.data import DataLoader
-from tqdm import tqdm
 from CNN import FawNet
-from Data_Loader import process_data, faw_batch_sampler, make_batches, FawDataset, faw_transform
+from Data_Loader import  FawDataset, faw_transform
 import pickle
+from services import process_data, faw_batch_sampler, make_batches, train_epochs
 import os
 
 def main():
-    ### test if cuda is connected:
-    # print(f' cuda  current device {torch.cuda.current_device()}')
-    #
-    # print(f' cuda device{torch.cuda.device(0)}')
-    #
-    # print(f' device count {torch.cuda.device_count()}')
-    #
-    # print(f' device name {torch.cuda.get_device_name(0)}')
-    #
-    # print(f' is cuda available {torch.cuda.is_available()}')
-    ###
-
-    # receiving user input
+    ##### receiving user input
     parser = argparse.ArgumentParser()
     parser.add_argument("reports_file", help="File path to the reports table")
     parser.add_argument("images_table_file", help="File path to the image table")
@@ -29,9 +17,8 @@ def main():
     parser.add_argument("outputs_directory", help="path directory where outputs will be saved")
     parser.add_argument("--is_gpu", help="path to images root directory", type=bool, default=False)
     parser.add_argument("-pb","--with_pbar", help="Should have progress bar", type=bool, default=False)
+    parser.add_argument("-pl","--print_losss", help="Should print loss while training", default=False)
     parser.add_argument("-bs","--bad_shapes", help="File with paths to bad images", default=None)
-
-
     args = parser.parse_args()
 
     reports_file = args.reports_file
@@ -40,12 +27,14 @@ def main():
     outputs_dir = args.outputs_directory
     is_gpu = args.is_gpu
     with_pbar = args.with_pbar
+    print_loss = args.print_loss
     bad_shape_images_path = args.bad_shapes
 
     if bad_shape_images_path is not None:
         with open(bad_shape_images_path, 'rb') as fp:
             bad_shape_images = pickle.load(fp)
     else: bad_shape_images = None
+    #######
 
     train_set_size = 0.75  # how many (out of 1) should be in the training set.
     test_set_size = 0.25
@@ -64,72 +53,27 @@ def main():
         reports_df, images_df, images_root_directory, bad_shape_images)
 
     ds = FawDataset(images=usable_reports_lst, labels=index_to_label, transform=faw_transform)
-
     all_batches = make_batches(batch_size, usable_reports_dic)
-
     train_until_index = int(len(all_batches) * train_set_size)
-
     train_sampler = faw_batch_sampler(all_batches[:train_until_index])
     test_sampler = faw_batch_sampler(all_batches[train_until_index:])
+    train_dl = DataLoader(ds, batch_sampler=train_sampler)
+    test_dl = DataLoader(ds, batch_sampler=test_sampler)
 
 ##save test and train indices in file to be used later
     with open(os.path.join(outputs_dir,"test_indices"),'wb') as f:
         pickle.dump(all_batches[train_until_index:], f)
-
     with open(os.path.join(outputs_dir,"train indices"),'wb') as f:
         pickle.dump(all_batches[:train_until_index], f)
+    #save test dl to be used in test
+    with open(os.path.join(outputs_dir,"test dl"),'wb') as f:
+        pickle.dump(test_dl, f)
 
-    train_dl = DataLoader(ds, batch_sampler=train_sampler)
-    test_dl = DataLoader(ds, batch_sampler=test_sampler)
     # the cnn
     model = FawNet()
     if is_gpu: model.cuda()
-    optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
-    criterion = torch.nn.MSELoss()
-
-    epoch_loss = 0.0
-    epochs_bar = tqdm(epochs, total=epochs, disable=(not with_pbar), desc="epochs", position=0,leave=True)
-    for epoch in range(epochs):
-        running_loss = 0.0
-        batches_bar = tqdm(train_until_index//batch_size, total=train_until_index//batch_size, disable=(not with_pbar), desc="batches in epoch", position=1,leave=True)
-        for i, data in enumerate(train_dl, 0):
-            inputs, labels = data
-            if is_gpu:
-                inputs = inputs.cuda()
-                labels = labels.cuda()
-
-            optimizer.zero_grad()
-            outputs = model(inputs)
-            outputs = outputs.flatten()
-            assert (labels.shape == outputs.shape)
-            loss = criterion(outputs, labels)
-            loss.backward()
-            optimizer.step()
-            running_loss += loss.item()
-            if (with_pbar):
-                batches_bar.update(n=1)
-            # if i == 1 :  # print every 100 mini-batches
-            #     print('[%d, %5d] loss: %.3f' %
-            #           (epoch + 1, i + 1, running_loss / 2000))
-            #     running_loss = 0.0
-            #     break
-        # print(f' loss for epoch {epoch} = {epoch_loss / train_until_index}')
-        if with_pbar:
-            epochs_bar.update(n=1)
-    print("finished training!")
+    train_epochs(model,train_dl,epochs,train_until_index,batch_size,with_pbar,print_loss)
     torch.save(model.state_dict(), os.path.join(outputs_dir,'faw_trained.pt'))
-
-
-def get_n_params(model):
-    pp = 0
-    for p in list(model.parameters()):
-        print(f'@@@@@@ {p.shape}')
-        nn = 1
-        for s in list(p.size()):
-            nn = nn * s
-        pp += nn
-    return pp
-
 
 if __name__ == '__main__':
     main()
