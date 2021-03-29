0e97d9530928dc2eaa31572074f6c4876040afbf
TzashR <tzachirotbain@gmail.com>
2021-03-29 16:30:15 +0300

    epochs in main instead train_and_test

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import argparse
 import os
 import pickle
+
 import torch.optim
 from torch.utils.data import DataLoader
+
 from CNN import FawNet
 from Data_Loader import make_ds_and_batches
 from services import faw_batch_sampler
-from train_and_test import train_epochs
+from train_and_test import train_epoch
+
 
 def main():
     ##### receiving user input
     parser = argparse.ArgumentParser()
-    parser.add_argument("epochs", help="how many epochs", type = int)
-    parser.add_argument("batch_size", help="batch size", type = int)
+    parser.add_argument("epochs", help="how many epochs", type=int)
+    parser.add_argument("batch_size", help="batch size", type=int)
     parser.add_argument("reports_file", help="File path to the reports table")
     parser.add_argument("images_table_file", help="File path to the image table")
     parser.add_argument("images_root_directory", help="path to directory where images are stored")
@@ -23,7 +26,8 @@ def main():
     parser.add_argument("-bs", "--bad_shaped_images", help="File with paths to bad images", default=None)
     args = parser.parse_args()
 
-    ds, all_batches = make_ds_and_batches(args.reports_file, args.images_table_file, args.images_root_directory, 'train',
+    ds, all_batches = make_ds_and_batches(args.reports_file, args.images_table_file, args.images_root_directory,
+                                          'train',
                                           bad_shape_images_path=args.bad_shaped_images, batch_size=args.batch_size)
 
     train_set_size = 0.75  # how many (out of 1) should be in the training set.
@@ -32,10 +36,6 @@ def main():
     assert (1 > test_set_size > 0 and 1 > train_set_size > 0 and train_set_size + test_set_size + val_set_size == 1)
 
     train_until_index = int(len(all_batches) * train_set_size)
-    train_sampler = faw_batch_sampler(all_batches[:train_until_index])
-    test_sampler = faw_batch_sampler(all_batches[train_until_index:])
-    train_dl = DataLoader(ds, batch_sampler=train_sampler)
-    test_dl = DataLoader(ds, batch_sampler=test_sampler)
 
     ##save test and train indices in file to be used later
     with open(os.path.join(args.outputs_dir, "test_indices"), 'wb') as f:
@@ -47,11 +47,23 @@ def main():
     model = FawNet()
     if args.is_gpu: model.cuda()
     ## train the model
-    train_epochs(model=model, train_dl=train_dl, num_epochs=args.epochs, train_until_index=train_until_index,
-                 batch_size=args.batch_size, with_pbar=args.with_pbar, print_loss=args.print_loss, is_gpu=args.is_gpu)
+    for epoch in range(args.epochs):
+        train_sampler = faw_batch_sampler(all_batches[:train_until_index])
+        train_dl = DataLoader(ds, batch_sampler=train_sampler)
+
+        if args.with_pbar: print(f'epoch #{epoch + 1}')
+        epoch_loss = train_epoch(model=model, train_dl=train_dl, train_until_index=train_until_index,
+                                  batch_size=args.batch_size, with_pbar=args.with_pbar, print_loss=args.print_loss,
+                                  is_gpu=args.is_gpu)
+        if args.print_loss:
+            print(f' loss for epoch {epoch} = {epoch_loss / train_until_index}')
+    print("finished training!")
+
     ##save the trained model
     torch.save(model.state_dict(), os.path.join(args.outputs_dir, 'faw_trained.pt'))
     # test the model
+    # test_sampler = faw_batch_sampler(all_batches[train_until_index:])
+    # test_dl = DataLoader(ds, batch_sampler=test_sampler)
     # test_model(model,test_dl, args.outputs_dir)
 
 
