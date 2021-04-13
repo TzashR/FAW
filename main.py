461137a0737819a9cd8515ca61a774867ae1b2e8
TzashR <tzachirotbain@gmail.com>
2021-04-13 16:16:21 +0300

    no gpu

--- main.py
+++ main.py
@@ -49,7 +49,8 @@ def main():
         pickle.dump(all_batches[:train_until_index], f)
 
     # the cnn
-    with_gpu =torch.cuda.is_available()
+    # with_gpu =torch.cuda.is_available()
+    with_gpu = False
 
     if with_gpu:
         device = torch.device("cuda:0")
